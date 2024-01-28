3fafca0856ff76f3116b475b458a5f64e6a48501
Artur Jordão <arturjordao@users.noreply.github.com>
2024-01-28 10:27:20 -0300

    Update search.py
    Code instruction

--- search.py
+++ search.py
@@ -3,10 +3,10 @@ import numpy as np
 if __name__ == '__main__':
     papers = np.load('Conferences/ICLR2024.npz', allow_pickle=True)['papers']
 
-    words = ['CKA']
+    words = ['CKA']#Put the key works here. For example, words = ['CKA', 'similarity']
     words = [x.lower() for x in words]
 
-    th = 15
+    th = 10#Frequency of keywords throughout the text.
     for paper in papers:
         file, content = paper[0], paper[1]
         hit = 0
